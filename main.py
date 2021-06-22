66e34a834c43f8d9b34cfc40ff5b49dfcbdc2a61
samadii <77415087+samadii@users.noreply.github.com>
2021-06-22 12:21:45 +0430

    Update and rename main_bot.py to main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-app_id = 
+app_id = int(os.environ.get("API_ID", 12345))
 app_key = os.environ.get('API_HASH')
 token = os.environ.get('BOT_TOKEN')
 
